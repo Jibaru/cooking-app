259f7afed83f1cba7c72fa721b2279dea4e73211
jibaru <ignacioruedaboada@hotmail.com>
2020-10-03 23:53:38 -0500

    Router: refactored

--- lib/utilities/router.dart
+++ lib/utilities/router.dart
@@ -1,27 +1,23 @@
-
-import 'package:cooking_app/screens/main_screen.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:cooking_app/screens/login_screen.dart';
+import 'package:cooking_app/screens/home_screen.dart';
+import 'package:cooking_app/screens_old/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:cooking_app/screens_old/login_screen.dart';
 
 typedef RouteFactory = Route<dynamic> Function(RouteSettings settings);
 
-class Router {
-
-  static final Router _router = Router._internal();
-
-  final Map<String, WidgetBuilder> _routes = {
-    '/login': (BuildContext context) => LoginScreen(),
-    '/main': (BuildContext context) => MainScreen()
+abstract class Router {
+  static final Map<String, String> routesNamed = {
+    '/login': 'Iniciar Sesión',
+    '/main': 'Principal',
+    '/home': 'Inicio'
   };
 
-  Map<String, WidgetBuilder> get routes => _routes;
+  static final Map<String, WidgetBuilder> routes = {
+    '/login': (BuildContext context) => LoginScreen(),
+    '/main': (BuildContext context) => MainScreen(),
+    '/home': (BuildContext context) => HomeScreen()
+  };
 
-  WidgetBuilder get unknownRouteSolver => _routes['login'];
-
-  factory Router() {
-    return _router;
-  }
-
-  Router._internal();
-
+  static Future<void> pushVoid(BuildContext context, String route) =>
+      Navigator.push(context, MaterialPageRoute(builder: routes[route]));
 }
