08b05e45b6091ddac2edcdb51ab23e23530fb713
jibaru <ignacioruedaboada@hotmail.com>
2020-10-06 18:58:03 -0500

    AppRaisedButton: Created raised button customized

---
+++ lib/widgets/app_raised_button.dart
@@ -0,0 +1,43 @@
+import 'package:cooking_app/utilities/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppRaisedButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+
+  const AppRaisedButton({
+    Key key,
+    @required this.text,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: CustomColors.blue,
+      highlightElevation: 0.5,
+      shape: Border.all(
+        style: BorderStyle.solid,
+        color: Colors.black12,
+        width: 0.1,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(
+          25.0,
+          10.0,
+          25.0,
+          10.0,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'ReemKufi',
+            fontSize: 18,
+          ),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
