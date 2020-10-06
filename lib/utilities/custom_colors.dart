73ea7a7c713e4e7f083ab96d31cada30807bad05
jibaru <ignacioruedaboada@hotmail.com>
2020-10-06 14:48:02 -0500

    CustomColors: Added blue custom color

--- lib/utilities/custom_colors.dart
+++ lib/utilities/custom_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-
   static const Color yellow = Color.fromRGBO(255, 165, 51, 1);
 
   static const Color yellow50 = Color.fromRGBO(255, 254, 235, 1);
 
+  static const Color blue = Color.fromRGBO(63, 88, 171, 1);
 }
