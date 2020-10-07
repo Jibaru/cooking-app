3cc4b7d549eeca1cccccdc7c8c525fc1c847a012
jibaru <ignacioruedaboada@hotmail.com>
2020-10-07 00:27:34 -0500

    HomeScreen: Some changes to make it work other screens and drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,20 @@
 import 'package:cooking_app/utilities/router.dart';
+import 'package:cooking_app/widgets/app_drawer.dart';
 import 'package:cooking_app/widgets/list_recipe.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _homeScreenScaffoldKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _homeScreenScaffoldKey,
       appBar: AppBar(
         title: Text('Recetas'),
         leading: IconButton(
           icon: Icon(Icons.menu),
-          // TODO: Add function to open sidebar
-          onPressed: () => {},
+          onPressed: () => _homeScreenScaffoldKey.currentState.openDrawer(),
         ),
         actions: <Widget>[
           IconButton(
@@ -20,7 +23,7 @@ class HomeScreen extends StatelessWidget {
             onPressed: () => {},
           ),
           PopupMenuButton(
-            onSelected: (value) => Router.pushVoid(context, value),
+            onSelected: (value) => Navigator.pushNamed(context, value),
             offset: Offset(10.0, 5.0),
             itemBuilder: (context) {
               return Router.routesNamed.keys
@@ -33,7 +36,9 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ListRecipe(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         // TODO: Add function to redirect to create recipe screen
