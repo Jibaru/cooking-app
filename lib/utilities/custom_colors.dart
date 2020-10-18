1d88c7c48ac842b2f7f65b01f33384b502f3dd6a
jibaru <ignacioruedaboada@hotmail.com>
2020-10-17 22:46:50 -0500

    CustomColors: Added blue50 color

--- lib/utilities/custom_colors.dart
+++ lib/utilities/custom_colors.dart
@@ -6,4 +6,6 @@ class CustomColors {
   static const Color yellow50 = Color.fromRGBO(255, 254, 235, 1);
 
   static const Color blue = Color.fromRGBO(63, 88, 171, 1);
+
+  static const Color blue50 = Color.fromRGBO(63, 88, 171, 0.5);
 }
