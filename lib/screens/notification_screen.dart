8ad7812c7069ec3ac18819d8304da4e1412c9d25
jibaru <ignacioruedaboada@hotmail.com>
2020-10-17 22:46:15 -0500

    NotificationScreen: Created notification screen

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -50,7 +50,52 @@ class NotificationListView extends StatelessWidget {
       itemCount: 5,
       itemBuilder: (context, index) {
         return NotificationCard(
-          onTap: () {},
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return SimpleDialog(
+                  title: Text('Título de la notificación'),
+                  titleTextStyle: TextStyle(
+                    color: Colors.black87,
+                    fontFamily: 'ReemKufi',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  children: <Widget>[
+                    Text(
+                      '17 de Octubre del 2020',
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontFamily: 'ReemKufi',
+                        fontSize: 13,
+                      ),
+                    ),
+                    Divider(
+                      indent: 10.0,
+                      endIndent: 10.0,
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                      style: TextStyle(
+                        fontFamily: 'Arial',
+                        fontSize: 14,
+                      ),
+                    ),
+                    Divider(
+                      indent: 10.0,
+                      endIndent: 10.0,
+                    ),
+                  ],
+                );
+              },
+            );
+          },
         );
       },
     );
