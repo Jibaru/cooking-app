5934310a97c1dac847315b80f55c02647f775312
jibaru <ignacioruedaboada@hotmail.com>
2020-10-28 00:51:40 -0500

    Router: Added routes to new screens

--- lib/presentation/utils/router.dart
+++ lib/presentation/utils/router.dart
@@ -7,6 +7,10 @@ import '../screens/recipe_create_screen.dart';
 import '../screens/setting_screen.dart';
 import '../screens/signin_screen.dart';
 import '../screens/login_screen.dart';
+import '../screens/recipe/favorite_recipes_screen.dart';
+import '../screens/recipe/my_recipes_screen.dart';
+import '../screens/recipe/stored_recipes_screen.dart';
+import '../screens/recipe/downloaded_recipes_screen.dart';
 
 typedef RouteFactory = Route<dynamic> Function(RouteSettings settings);
 
@@ -25,6 +29,21 @@ abstract class Router {
     '/setting': (BuildContext context) => SettingScreen(),
     '/profile': (BuildContext context) => ProfileScreen(),
     '/notification': (BuildContext context) => NotificationScreen(),
-    '/recipe-create': (BuildContext context) => RecipeCreateScreen()
+    '/recipe-create': (BuildContext context) => RecipeCreateScreen(),
+    '/favorite-recipes': (BuildContext context) => FavoriteRecipesScreen(),
+    '/my-recipes': (BuildContext context) => MyRecipesScreen(),
+    '/stored-recipes': (BuildContext context) => StoredRecipesScreen(),
+    '/downloaded-recipes': (BuildContext context) => DownloadedRecipesScreen(),
   };
+
+  static bool isCurrent(BuildContext context, String routeName) {
+    bool isCurrent = false;
+    Navigator.of(context).popUntil((route) {
+      if (route.settings.name == routeName) {
+        isCurrent = true;
+      }
+      return true;
+    });
+    return isCurrent;
+  }
 }
