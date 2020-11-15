97327f22fab730518592e1387f8468e4a10ef04f
jibaru <ignacioruedaboada@hotmail.com>
2020-11-15 15:58:05 -0500

    Main: Added DotEnv load

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'package:cooking_app/presentation/app.dart';
+import './presentation/app.dart';
 
-void main() {
+void main() async {
+  await DotEnv().load('.env');
   runApp(App());
 }
