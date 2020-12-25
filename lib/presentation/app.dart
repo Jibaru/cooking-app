53e2309448c4f19be30f1dbf7f9df780025967b0
jibaru <ignacioruedaboada@hotmail.com>
2020-12-24 19:39:46 -0500

    App: Added authentication and user blocs

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,30 +1,36 @@
-import 'package:dio/dio.dart';
+// import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../blocs/all_recipes/all_recipes_bloc.dart';
-import '../data/repositories/recipe/recipe_repository.dart';
-import '../data/repositories/recipe/api_recipe_repository.dart';
+import '../blocs/blocs.dart';
+import '../data/repositories/repositories.dart';
 
 import './utils/app_theme.dart';
 import 'utils/app_router.dart';
 
 class App extends StatelessWidget {
-  final RecipeRepository recipeRepository = ApiRecipeRepository(Dio());
+  // final RecipeRepository recipeRepository = ApiRecipeRepository(Dio());
+  final UserRepository userRepository = ApiUserRepository();
 
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
         RepositoryProvider(
-          create: (context) => recipeRepository,
+          create: (context) => userRepository,
         ),
       ],
       child: MultiBlocProvider(
         providers: [
-          BlocProvider<AllRecipesBloc>(
-            create: (context) =>
-                AllRecipesBloc(apiRecipeRepository: recipeRepository),
+          // BlocProvider<AllRecipesBloc>(
+          //   create: (context) =>
+          //       AllRecipesBloc(apiRecipeRepository: recipeRepository),
+          // ),
+          BlocProvider<AuthenticationBloc>(
+            create: (context) => AuthenticationBloc(userRepository: userRepository),
+          ),
+          BlocProvider<UserBloc>(
+            create: (context) => UserBloc(userRepository: userRepository),
           ),
         ],
         child: MaterialApp(
