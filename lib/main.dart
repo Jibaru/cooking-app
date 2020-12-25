616ab63e2df748b723865cbc39773525045de807
jibaru <ignacioruedaboada@hotmail.com>
2020-12-24 19:38:53 -0500

    Main: Added UserRepository and load prefs user

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'data/repositories/repositories.dart';
 import './presentation/app.dart';
 
 void main() async {
+  final UserRepository userRepository = ApiUserRepository();
   await DotEnv().load('.env');
+  await userRepository.loadPrefsUser();
   runApp(App());
 }
